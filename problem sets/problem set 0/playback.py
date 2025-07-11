2e05b3bd24e3440e41d64f0d1e642bc7a3057b4c
Adelaidemhuntana <163135758+Adelaidemhuntana@users.noreply.github.com>
2025-07-11 19:21:09 +0200

    Add Problem Set 0 solution for playback.py

--- problem sets/problem set 0/playback.py
+++ problem sets/problem set 0/playback.py
@@ -1,11 +1,5 @@
-def convert(emoticons):
-    emoticons = emoticons.replace(":)", "🙂")  
-    emoticons = emoticons.replace(":(", "🙁")  
-    return emoticons
-
 def main():
-    user_text = input("Type something: ")
-    result = convert(user_text)
-    print(result)
+    text = input()
+    print(text.replace(" ", "..."))
 
 main()
